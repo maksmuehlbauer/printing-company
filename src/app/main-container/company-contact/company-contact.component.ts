041f95edff0c9548619a0c1b1e8409e9e1018ad0
maksmuehlbauer <nakeda1987@gmail.com>
2024-08-19 20:19:19 +0200

    add contact validation

--- src/app/main-container/company-contact/company-contact.component.ts
+++ src/app/main-container/company-contact/company-contact.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-company-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './company-contact.component.html',
   styleUrl: './company-contact.component.scss'
 })
 export class CompanyContactComponent {
+
+  isDisabled: boolean = true;
+
+  contactData = {
+    name: "",
+    mail: "",
+    message: ""
+  }
+
+  onSubmit(ngForm: NgForm) {
+    console.log(this.contactData)
+  }
+
+  checkInputs() {
+    if(this.contactData.name.length > 1 &&
+      this.contactData.mail.length > 5 &&
+      this.contactData.message.length > 9) {
+        this.isDisabled = false
+    } else {
+      this.isDisabled = true;
+    }
+
+  }
+
+  btnState() {
+    return this.isDisabled ? 'btn-disabled' : 'btn-submit'
+  }
 
 }
