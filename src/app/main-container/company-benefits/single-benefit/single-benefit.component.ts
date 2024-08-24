66da48494f8b060341431b806225a4bf744fa0d1
maksmuehlbauer <nakeda1987@gmail.com>
2024-08-24 12:12:20 +0200

    add navDesk & hovers

--- src/app/main-container/company-benefits/single-benefit/single-benefit.component.ts
+++ src/app/main-container/company-benefits/single-benefit/single-benefit.component.ts
@@ -1,18 +1,27 @@
 import { Component, Input } from '@angular/core';
 import { CompanyService } from '../../../company.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-benefit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-benefit.component.html',
   styleUrl: './single-benefit.component.scss'
 })
 export class SingleBenefitComponent {
+
 
   @Input()benefit =  {
     img: 'img path',
     title: 'title',
     description: 'description'
   }
+
+  @Input() id!: number;
+
+  oddIdsColor(id: number): string {
+    return id % 2 !== 0 ? 'special-odd-id' : ''
+  }
+
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+ menuVisible: boolean = false;
+//  scrollEnabled: boolean = true;
+  
+menuToggle() {
+  if(!this.menuVisible) {
+    this.menuVisible = true;
+    // this.toggleScrolling()
+  } else {
+    this.menuVisible = false;
+    // this.toggleScrolling()
+  }
+}
+
+// toggleScrolling() {
+//   if (this.scrollEnabled) {
+//       document.body.style.overflow = 'hidden';
+//       this.scrollEnabled = false;
+//   } else {
+//       document.body.style.overflow = 'auto';
+//       this.scrollEnabled = true;
+//   }
+
+//   }
 }
